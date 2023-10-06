0b2a44897b16f40b94321ef6e31d608600b5eaf3
Divya Gadekar <116143709+divya-gadekar28@users.noreply.github.com>
2023-10-06 22:08:41 +0530

    Create program4.java

---
+++ program4.java
@@ -0,0 +1,69 @@
+// Java Program to Check If a Number is Neon number or not
+import java.io.*;
+ 
+class Main{
+ 
+    // Method to check whether number is neon or not
+    // Boolean type
+    public static boolean checkNeon(int n)
+    {
+        // squaring the number to be checked
+        int square = n * n;
+ 
+        // Initializing current sum to 0
+        int sum = 0;
+ 
+        // If product is positive
+        while (square > 0) {
+ 
+            // Step 1: Find remainder
+            int r = square % 10;
+ 
+            // Add remainder to the current sum
+            sum += r;
+ 
+            // Drop last digit of the product
+            // and store the number
+            square = square / 10;
+        }
+ 
+        // Condition check
+        // Sum of digits of number obtained is
+        // equal to original number
+        if (sum == n)
+ 
+            // number is neon
+            return true;
+        else
+ 
+            // number is not neon
+            return false;
+    }
+ 
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Custom input
+        int n = 9;
+ 
+        // Calling above function to check custom number or
+        // if user entered number via Scanner class
+        if (checkNeon(n))
+ 
+            // Print number considered is neon
+            System.out.println("Given number " + n
+                               + " is Neon number");
+        else
+ 
+            // Print number considered is not neon
+            System.out.println("Given number " + n
+                               + " is not a Neon number");
+    }
+
+//Output:-
+//Input  : 9
+//Output : Given number  9 is Neon number
+
+//Explanation : square of 9=9*9=81;
+//sum of digit of square : 8+1=9(which is equal to given number therefore it is neon number else not)
+  
